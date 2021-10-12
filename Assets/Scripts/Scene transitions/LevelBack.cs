59ce91a38dafe47daf1ab852d17ee3ab562d7391
Yashkokane <37013378+Yashkokane@users.noreply.github.com>
2021-10-12 17:48:44 +0100

    camera swaps

--- Assets/Scripts/Scene transitions/LevelBack.cs
+++ Assets/Scripts/Scene transitions/LevelBack.cs
@@ -15,26 +15,15 @@ public class LevelBack : MonoBehaviour
     
     public bool lvl2Cam;
     [SerializeField] private CinemachineVirtualCamera vCam1;
-
     [SerializeField] private CinemachineVirtualCamera vCam2;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            //player.transform.position = new Vector3(71.38f, 172.82f, 0f);
             player.transform.position = playerInitalPosition.transform.position;
-            /*player.transform.position = playerInitalPosition;
-            SceneManager.LoadScene(SceneToLoad);*/
-            
-            /*pM.Level2 = true;
-            pM.Level1 = false;*/
-            //pM.onSpawn();
-            //if (lvl2Cam)
-            
-                vCam2.Priority = 8;
-                vCam1.Priority = 9;
-            
-            //lvl2Cam = !lvl2Cam; 
+            vCam2.Priority = 8;
+            vCam1.Priority = 9;
+     
 
         }
     }
